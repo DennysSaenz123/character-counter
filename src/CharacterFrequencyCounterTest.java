31a1dad817116fc877981472e65788bc6ba6a928
DennysSaenz <SaenzSegovia.Dennys@student.greenriver.edu>
2026-04-16 11:14:17 -0700

    Made tests for characterCounter

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    void testcharactershowsupmultipletimesgivecorrectfrequency() {
+        //Arrange 
+        CharacterFrequencyCounter counter  = new CharacterFrequencyCounter("hello");
+        //Act 
+        int actual = counter.getFrequency('l');
+        //Assert
+        assertEquals(2, actual);
+    }
+
+    @Test 
+    void testGetRelativeFrequencyREturns0ForMissingCharacter() {
+        //Arrange 
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        //Act 
+        double actual = counter.getRelativePercentage('x');
+        //Assert
+        assertEquals(40.0, actual, 0.0001);
+    }
 }
